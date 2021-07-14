8a2eb2265921547af487f63aa981ae347b24a7bb
Raymund Santillan <raymsantillan@gmail.com>
2021-07-15 00:54:25 +0800

    add cors support for frontend

--- go.mod
+++ go.mod
@@ -9,8 +9,10 @@ go 1.14
 require (
 	babelist.ph/api/config/database v0.0.0-00010101000000-000000000000
 	babelist.ph/api/models/babe v0.0.0-00010101000000-000000000000
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/rs/cors v1.8.0 // indirect
 	gorm.io/driver/mysql v1.0.4 // indirect
 	gorm.io/driver/sqlite v1.1.4 // indirect
 	gorm.io/gorm v1.20.12 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"babelist.ph/api/models/babe"
 	"babelist.ph/api/config/database"
 )
@@ -27,8 +28,17 @@ func initRequestHandler() {
 	router.HandleFunc("/api/v1/babes", babe.GetAll)
 	router.HandleFunc("/api/v1/babes/{id}", babe.Find)
 	router.HandleFunc("/api/v1/babes/{id}", babe.Update).Methods("PUT")
+
+	// set the headers
+	 c := cors.New(cors.Options{
+        AllowedOrigins: []string{"*"},
+        AllowCredentials: true,
+    })
+
+    handler := c.Handler(router)
+
 	// start the webserver
-    log.Fatal(http.ListenAndServe(":8080", router))
+    log.Fatal(http.ListenAndServe(":8000", handler ))
 
 }
 
